9b9ea9a3f87d64cfd1837442c2064fd7a76ac86f
yadav <yadavanand7441@gmail.com>
2025-12-07 17:28:31 +0530

    commit

--- src/main/java/com/spring/with/jpa/boot/entity/Employee.java
+++ src/main/java/com/spring/with/jpa/boot/entity/Employee.java
@@ -1,8 +1,9 @@
 package com.spring.with.jpa.boot.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "employee_database")
